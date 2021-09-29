c13717fdc8b2f8541c550e216bf25b828c52b21d
Natanael <NatanaelUF17@gmail.com>
2021-09-28 22:03:47 -0400

    Finishing day 1 of learning swift 🤩

---
+++ day-1/day1.playground/Contents.swift
@@ -0,0 +1,58 @@
+import UIKit
+
+// 1. Variables
+var greeting = "Hello, Natanael"
+greeting = "Hello, Github"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+// 2. Strings and integers
+var age = 21
+var population = 1_000_000_000
+
+// 3. Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+var burns = """
+The best laid \
+0' mice and men \
+Gang aft agley
+"""
+
+// 4. Doubles and Booleans
+var pi = 3.141
+var price = 29.99
+var isMarried = false
+var isAwesome = true
+
+// 5. String interpolation
+var score = 85
+var scoreMessage = "Your score was \(score)"
+
+var results = "The test results are here: \(scoreMessage)"
+
+var city = "San Francisco"
+var cityMessage = "Welcome to \(city)"
+
+// 6. Constants
+let tom = "holland"
+let johnny = "depp"
+
+// 7. Type annotations
+let str = "Hello, playground" // type inference
+let album: String = "Life and Death"
+let year: Int = 1999
+let height: Double = 1.78
+let hasSuperpowers = true
